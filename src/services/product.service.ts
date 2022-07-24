db9f18cff83dd274127a17d80e26b071c051419d
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-07-24 22:47:01 +0700

    create product services

---
+++ src/services/product.service.ts
@@ -0,0 +1,61 @@
+import mongoose, { DocumentDefinition } from "mongoose";
+import ProductModel, { ProductDocument } from "../models/product.model";
+import logger from "../utils/logger.utils";
+
+export async function createProductService(
+  input: DocumentDefinition<Omit<ProductDocument, "createdAt" | "updatedAt">>
+) {
+  try {
+    return await ProductModel.create(input);
+  } catch (error: any) {
+    logger.error(error);
+    throw new Error(error);
+  }
+}
+
+export async function findProductService(
+  query: mongoose.FilterQuery<ProductDocument>,
+  opts: mongoose.QueryOptions = { lean: true }
+) {
+  try {
+    return await ProductModel.find(query, {}, opts);
+  } catch (error: any) {
+    logger.error(error);
+    throw new Error(error);
+  }
+}
+
+export async function findOneProductService(
+  query: mongoose.FilterQuery<ProductDocument>,
+  opts: mongoose.QueryOptions
+) {
+  try {
+    return await ProductModel.findOne(query, {}, opts);
+  } catch (error: any) {
+    logger.error(error);
+    throw new Error(error);
+  }
+}
+
+export async function finAndUpdateProductService(
+  query: mongoose.FilterQuery<ProductDocument>,
+  update: DocumentDefinition<Omit<ProductDocument, "createdAt" | "updatedAt">>,
+  options: mongoose.QueryOptions
+) {
+  try {
+    return await ProductModel.findOneAndUpdate(query, update, options);
+  } catch (error: any) {
+    logger.error(error);
+    throw new Error(error);
+  }
+}
+export async function deleetProductService(
+  query: mongoose.FilterQuery<ProductDocument>
+) {
+  try {
+    return await ProductModel.deleteOne(query);
+  } catch (error: any) {
+    logger.error(error);
+    throw new Error(error);
+  }
+}
